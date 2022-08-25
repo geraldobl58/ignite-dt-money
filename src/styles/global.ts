8cf6643695291d65903866eb9b88032c378cfd7b
Geraldo Luiz <geraldobl58@gmail.com>
2022-08-25 16:27:21 -0300

    :sparkles: Creating header component

--- src/styles/global.ts
+++ src/styles/global.ts
@@ -8,13 +8,13 @@ export const GlobalStyle = createGlobalStyle`
 
     :focus {
       outline: 0;
-      box-shadow: 0 0 0 2px ${props => props.theme['gray-500']};
+      box-shadow: 0 0 0 2px ${(props) => props.theme['gray-500']};
     }
   }
 
   body {
-    color: ${props => props.theme['gray-100']};
-    background-color: ${props => props.theme['gray-800']};
+    color: ${(props) => props.theme['gray-100']};
+    background-color: ${(props) => props.theme['gray-800']};
     -webkit-font-smoothing: antialiased;
   }
 
